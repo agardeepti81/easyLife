f94bb48d9f9b7b85484cf7710d340325b0f89c3f
Deepti Agarwal <deepti7206@gmail.com>
2023-03-01 22:28:46 -0500

    commandLineRunner edited

--- src/main/java/org/perscholas/easylife/MyCommandLineRunner.java
+++ src/main/java/org/perscholas/easylife/MyCommandLineRunner.java
@@ -4,12 +4,15 @@ import jakarta.annotation.PostConstruct;
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
+import org.perscholas.easylife.dao.AuthGroupRepoI;
 import org.perscholas.easylife.dao.CategoriesRepoI;
 import org.perscholas.easylife.dao.ItemsRepoI;
 import org.perscholas.easylife.dao.UsersRepoI;
+import org.perscholas.easylife.models.AuthGroup;
 import org.perscholas.easylife.models.Categories;
 import org.perscholas.easylife.models.Items;
 import org.perscholas.easylife.models.Users;
+import org.perscholas.easylife.services.UserServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -18,17 +21,19 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class MyCommandLineRunner implements CommandLineRunner {
-    ItemsRepoI items;
-    UsersRepoI users;
-    CategoriesRepoI categories;
-
+    ItemsRepoI itemsRepoI;
+    UsersRepoI usersRepoI;
+    CategoriesRepoI categoriesRepoI;
+    UserServices userServices;
+    AuthGroupRepoI authGroupRepoI;
 
     @Autowired
-    public MyCommandLineRunner(UsersRepoI users, CategoriesRepoI categories, ItemsRepoI items) {
-        this.items = items;
-        this.users = users;
-        this.categories = categories;
-
+    public MyCommandLineRunner(ItemsRepoI items, UsersRepoI users, CategoriesRepoI categories, UserServices userServices, AuthGroupRepoI authGroupRepoI) {
+        this.itemsRepoI = items;
+        this.usersRepoI = users;
+        this.categoriesRepoI = categories;
+        this.userServices = userServices;
+        this.authGroupRepoI = authGroupRepoI;
     }
 
     @PostConstruct
@@ -37,22 +42,38 @@ public class MyCommandLineRunner implements CommandLineRunner {
     }
     @Override
     public void run(String... args) throws Exception {
-        Users user1 = new Users("John Doe","johndoe@gmail.com","passWORD@12");
-        Users user2 = new Users("Natalie Bennet","nbennet@msn.com","passWORD@12");
-        Users user3 = new Users("Ron Weasley","rweasley@yahoo.com","passWORD@12");
-        users.save(user1);
-        users.save(user2);
-        users.save(user3);
+
+        Users user1 = new Users("Deepti Agarwal","deeptiag@gmail.com","passWORD@12");
+        Users user2 = new Users("John Doe","johndoe@gmail.com","passWORD@12");
+        Users user3 = new Users("Natalie Bennet","nbennet@msn.com","passWORD@12");
+        Users user4 = new Users("Ron Weasley","rweasley@yahoo.com","passWORD@12");
+
+        usersRepoI.save(user1);
+        usersRepoI.save(user2);
+        usersRepoI.save(user3);
+        usersRepoI.save(user4);
+
+        AuthGroup authUser1 = new AuthGroup("deeptiag@gmail.com","ROLE_ADMIN");
+        AuthGroup authUser2 = new AuthGroup("deeptiag@gmail.com","ROLE_USER");
+        AuthGroup authUser3 = new AuthGroup("johndoe@gmail.com","ROLE_USER");
+        AuthGroup authUser4 = new AuthGroup("nbennet@msn.com","ROLE_USER");
+        AuthGroup authUser5 = new AuthGroup("nbennet@msn.com","ROLE_USER");
+
+        authGroupRepoI.save(authUser1);
+        authGroupRepoI.save(authUser2);
+        authGroupRepoI.save(authUser3);
+        authGroupRepoI.save(authUser4);
+        authGroupRepoI.save(authUser5);
 
         Categories category1 = new Categories("Grocery");
         Categories category2 = new Categories("Electrical");
         Categories category3 = new Categories("Lawn and Garden");
         Categories category4 = new Categories("Kitchen and Dining");
 
-        categories.save(category1);
-        categories.save(category2);
-        categories.save(category3);
-        categories.save(category4);
+        categoriesRepoI.save(category1);
+        categoriesRepoI.save(category2);
+        categoriesRepoI.save(category3);
+        categoriesRepoI.save(category4);
 
         Items item1 = new Items("Rice",20,"lbs");
         Items item2 = new Items("Sugar",5,"lbs");
@@ -64,57 +85,19 @@ public class MyCommandLineRunner implements CommandLineRunner {
         Items item8 = new Items("Table Mats",4,"unit");
         Items item9 = new Items("Rice",15,"lbs");
 
-        items.save(item1);
-        items.save(item2);
-        items.save(item3);
-        items.save(item4);
-        items.save(item5);
-        items.save(item6);
-        items.save(item7);
-        items.save(item8);
-        items.save(item9);
+        itemsRepoI.save(item1);
+        itemsRepoI.save(item2);
+        itemsRepoI.save(item3);
+        itemsRepoI.save(item4);
+        itemsRepoI.save(item5);
+        itemsRepoI.save(item6);
+        itemsRepoI.save(item7);
+        itemsRepoI.save(item8);
+        itemsRepoI.save(item9);
 
-        //User1 item and category adding process
-        category1.addItems(item1);
-        category1.addItems(item2);
-        category2.addItems(item3);
-        categories.save(category1);
-        categories.save(category2);
-        user1.addCategory(category1);
-        user1.addCategory(category2);
-        user1.addItems(item1);
-        user1.addItems(item2);
-        user1.addItems(item3);
-        users.save(user1);
-
-        //User2 Item and Category adding process
-        user2.addCategory(category1);
-        user2.addCategory(category2);
-        user2.addCategory(category3);
-        user2.addItems(item9);
-        user2.addItems(item4);
-        user2.addItems(item5);
-        users.save(user2);
-        category1.addItems(item9);
-        category2.addItems(item4);
-        category3.addItems(item5);
-        categories.save(category1);
-        categories.save(category2);
-        categories.save(category3);
-
-        //User3 Item and Category adding process
-        user3.addCategory(category3);
-        user3.addCategory(category4);
-        user3.addItems(item6);
-        user3.addItems(item7);
-        user3.addItems(item8);
-        users.save(user3);
-        category3.addItems(item6);
-        category4.addItems(item7);
-        category4.addItems(item8);
-        categories.save(category3);
-        categories.save(category4);
-
+        userServices.addCategoriesToUser(user2);
+        userServices.addCategoriesToUser(user3);
+        userServices.addCategoriesToUser(user4);
    }
 }
 
--- src/main/java/org/perscholas/easylife/service/ItemsService.java
+++
@@ -1,5 +0,0 @@
-package org.perscholas.easylife.service;
-
-public class ItemsService {
-
-}
